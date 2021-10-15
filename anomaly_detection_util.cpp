928aeed98436bbc10f8aa03d6b4917cf4f3b0ae4
Peleg Shlomo <pelegs29@gmail.com>
2021-10-15 14:11:44 +0300

    meanCalc and covariance func added a CmakeList txt

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include "anomaly_detection_util.h"
+
+using namespace std;
 
 //Helper method to calculate mean or expected value
 float meanCalc(float *x, int size) {
@@ -15,4 +18,16 @@ float cov(float *x, float *y, int size) {
         sum += ((x[i] - meanCalc(x, size)) * (y[i] - meanCalc(y, size)));
     }
     return (1 / (size - 1)) * sum;
+}
+
+int main() {
+    float X[] = {65.21, 64.75, 65.26, 65.76, 65.96};
+    int sizeX = sizeof(X) / sizeof(X[0]);
+    float Y[] = {67.25, 66.39, 66.12, 65.70, 66.64};
+    int sizeY = sizeof(Y) / sizeof(Y[0]);
+
+    if (sizeX == sizeY){
+        cout << cov(X, Y, sizeY);
+    }
+    return 0;
 }
