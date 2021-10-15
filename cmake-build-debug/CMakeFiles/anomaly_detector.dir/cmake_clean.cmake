7ca0de9dc765f8e55a01ede34fb2a3394c6a469b
Peleg Shlomo <pelegs29@gmail.com>
2021-10-15 14:12:19 +0300

    meanCalc and covariance func added a CmakeList txt

---
+++ cmake-build-debug/CMakeFiles/anomaly_detector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anomaly_detector.dir/anomaly_detection_util.cpp.o"
+  "anomaly_detector"
+  "anomaly_detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/anomaly_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
