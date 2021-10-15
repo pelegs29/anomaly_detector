5321d6520a6e76161143387672a3acbdc71a466a
Peleg Shlomo <pelegs29@gmail.com>
2021-10-15 13:48:55 +0300

    meanCalc and covariance func

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,9 +1,18 @@
 #include "anomaly_detection_util.h"
 
-//Helper method to calculate expected value
+//Helper method to calculate mean or expected value
 float meanCalc(float *x, int size) {
     float sum = 0;
     for (int i = 0; i < size; ++i) {
-        
+        sum += x[i];
     }
+    return (1 / size) * sum;
+}
+
+float cov(float *x, float *y, int size) {
+    float sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += ((x[i] - meanCalc(x, size)) * (y[i] - meanCalc(y, size)));
+    }
+    return (1 / (size - 1)) * sum;
 }
