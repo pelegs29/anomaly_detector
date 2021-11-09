8fb9bc87020d5951225612bad4a6844a523b1548
Peleg <pelegs29@gmail.com>
2021-11-09 11:29:33 +0200

    created learnNormal method

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -1,11 +1,12 @@
-
+#include <memory>
 #include "SimpleAnomalyDetector.h"
 
-#define THRESHOLD 0.7;
+#define THRESHOLD 0.7
+using namespace std;
 
 correlatedFeatures::correlatedFeatures(string feature1, string feature2, float corrlation,
                                        const Line &linReg, float threshold) :
-        feature1(std::move(feature1)), feature2(std::move(feature2)),
+        feature1(move(feature1)), feature2(move(feature2)),
         corrlation(corrlation), lin_reg(linReg),
         threshold(threshold) {}
 
@@ -19,43 +20,57 @@ SimpleAnomalyDetector::~SimpleAnomalyDetector() {
     // TODO Auto-generated destructor stub
 }
 
+Point **createPointsArr(float *x, float *y, int size) {
+    auto **points_arr = new Point *[size];
+    for (int i = 0; i < size; ++i) {
+        points_arr[i] = new Point(x[i], y[i]);
+    }
+    return points_arr;
+}
+
+void freePointsArr(Point **arr, int size) {
+    for (int i = 0; i < size; ++i) {
+        delete arr[i];
+    }
+    delete arr;
+}
+
 //method to create vector of correlatedFeatures in the TimeSeries given
 void SimpleAnomalyDetector::learnNormal(const TimeSeries &ts) {
-    vector <correlatedFeatures> correlatedFound;
-    for (int i = 0; i < ts.getData().size(); ++i) {
+    vector<correlatedFeatures> correlatedFound;
+    vector<pair<string, vector<float>>> data = ts.getData();
+    // flag -> if no correlativeDuo found
+    int correlativeDuo = -1;
+    for (int i = 0; i < data.size(); ++i) {
         float currentMax = THRESHOLD;
+        //pResult will hold the current correlation between feature i and j
         float pResult;
-
-
-    }
-
-
-    auto outerIt = ts.data.begin();
-    auto innerIt = outerIt++;
-    while (outerIt != ts.data.end()) {
-        string correlativeDuo;
-        float currentMax = THRESHOLD;
-        float pResult;
-        while (innerIt != ts.data.end()) {
-            //TODO check if all the tables size are equal !
-            std::vector<float> vector1 = outerIt->second;
-            std::vector<float> vector2 = innerIt->second;
-            float *featureI = &vector1[0];
-            float *featureJ = &vector2[0];
-            int size = (int) outerIt->second.size();
-            pResult = fabs(pearson(featureI, featureJ, size));
+        float *featureIArray = &data[i].second[0];
+        int featureSize = (int) data[i].second.size();
+        for (int j = i + 1; j < data.size(); ++j) {
+            float *featureJArray = &data[j].second[0];
+            pResult = fabs(pearson(featureIArray, featureJArray, featureSize));
+            //if a better correlation found save the index and update the currentMax
             if (currentMax < pResult) {
                 currentMax = pResult;
-                correlativeDuo = innerIt->first;
+                correlativeDuo = j;
             }
         }
-        if (!correlativeDuo.empty())
-            //TODO write some signatures in header file for TimeSeries for use in linear_reg function
-            struct correlatedFeatures duoFound = correlatedFeatures(outerIt->first, innerIt->first, pResult,);
+        //if a correlation has been found, create a correlatedFeature and add it the SimpleAnomalyDetector vector
+        if (correlativeDuo != -1) {
+            Point **points_arr = createPointsArr(&data[i].second[0],
+                                                 &data[correlativeDuo].second[0], featureSize);
+            Line linearReg = linear_reg(points_arr, featureSize);
+            correlatedFeatures duoFound = correlatedFeatures(data[i].first,
+                                                             data[correlativeDuo].first,
+                                                             currentMax, linearReg, THRESHOLD);
+            this->cf.push_back(duoFound);
+            freePointsArr(points_arr, featureSize);
+        }
     }
 }
 
-vector <AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries &ts) {
+vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries &ts) {
     // TODO Auto-generated destructor stub
 }
 
